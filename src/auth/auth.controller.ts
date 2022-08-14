f2dd9dc70d3668a42a6f916a885d499a2902f905
han.junho <han.junho@yanolja.com>
2022-08-14 16:38:08 +0900

    feat: auth custom decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,14 @@ import {
   Body,
   Controller,
   Post,
-  Req,
   UseGuards,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -31,7 +32,7 @@ export class AuthController {
 
   @Post('/:test')
   @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log('req', req);
+  test(@GetUser() user: User) {
+    console.log('user', user);
   }
 }
