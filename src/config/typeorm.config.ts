dfb639a2706385545279606464fe1fee6e441e53
han.junho <han.junho@yanolja.com>
2022-08-25 20:13:26 +0900

    fix: config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,11 @@ const dbConfig = config.get('db');
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  host: process.env.RDS_HOSTNAME || dbConfig.host,
-  port: process.env.RDS_PORT || dbConfig.port,
-  username: process.env.RDS_USERNAME || dbConfig.username,
-  password: process.env.RDS_PASSWORD || dbConfig.password,
-  database: process.env.RDS_DB_NAME || dbConfig.database,
+  host: process.env.DB_HOST || dbConfig.host,
+  port: process.env.DB_PORT || dbConfig.port,
+  username: process.env.DB_USER || dbConfig.username,
+  password: process.env.DB_PASSWORD || dbConfig.password,
+  database: process.env.DB_DATABASE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'], // 엔티티를 이용해서 데이터 베이스 테이블을 생성
   synchronize: true, // 애플리케이션을 다시 실핼할 때 엔티티 안에서 수정된 컬럼의 변경등에 대해 해당 테이블을 Drop 후 다시 생성
 };
