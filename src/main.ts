a0f7e82d8b38c2276936931b758f208eee31a204
han.junho <han.junho@yanolja.com>
2022-08-27 19:47:38 +0900

    feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3030;
   app.enableCors();
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Study API Docs')
+    .setDescription('NestJS Study API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT || port);
   Logger.log(`Application running on port ${port}`);
 }
