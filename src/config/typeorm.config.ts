9e83a8a17a221b6a5b8bac8ede11b68e6269178b
han.junho <han.junho@yanolja.com>
2022-08-28 14:49:29 +0900

    feat: build config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,14 +3,26 @@ import * as config from 'config';
 
 const dbConfig = config.get('db');
 
-export const typeORMConfig: TypeOrmModuleOptions = {
-  type: dbConfig.type,
-  url: process.env.DATABASE_URL,
-  // host: process.env.DB_HOST || dbConfig.host,
-  // port: process.env.DB_PORT || dbConfig.port,
-  // username: process.env.DB_USER || dbConfig.username,
-  // password: process.env.DB_PASSWORD || dbConfig.password,
-  // database: process.env.DB_DATABASE || dbConfig.database,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'], // 엔티티를 이용해서 데이터 베이스 테이블을 생성
-  synchronize: true, // 애플리케이션을 다시 실핼할 때 엔티티 안에서 수정된 컬럼의 변경등에 대해 해당 테이블을 Drop 후 다시 생성
-};
+let typeConfig;
+
+if (process.env.NODE_ENV === 'development')
+  typeConfig = {
+    type: dbConfig.type,
+    // url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST || dbConfig.host,
+    port: process.env.DB_PORT || dbConfig.port,
+    username: process.env.DB_USER || dbConfig.username,
+    password: process.env.DB_PASSWORD || dbConfig.password,
+    database: process.env.DB_DATABASE || dbConfig.database,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'], // 엔티티를 이용해서 데이터 베이스 테이블을 생성
+    synchronize: true, // 애플리케이션을 다시 실핼할 때 엔티티 안에서 수정된 컬럼의 변경등에 대해 해당 테이블을 Drop 후 다시 생성
+  };
+else {
+  typeConfig = {
+    type: dbConfig.type,
+    url: process.env.DATABASE_URL,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'], // 엔티티를 이용해서 데이터 베이스 테이블을 생성
+    synchronize: true, // 애플리케이션을 다시 실핼할 때 엔티티 안에서 수정된 컬럼의 변경등에 대해 해당 테이블을 Drop 후 다시 생성
+  };
+}
+export const typeORMConfig: TypeOrmModuleOptions = typeConfig;
